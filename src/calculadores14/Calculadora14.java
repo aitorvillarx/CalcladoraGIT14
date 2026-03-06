cbfd610e74d4de14cb49eb47f228956c8c8c6ac7
Aitor <aitorvillarx@gmail.com>
2026-03-06 11:50:09 +0100

    Proyecto Calculadora GIT, metodo sumar y restar, multiplicar y dividir

--- src/calculadores14/Calculadora14.java
+++ src/calculadores14/Calculadora14.java
@@ -4,4 +4,11 @@ public class Calculadora14 {
 	public int restar(int a, int b) {
 		 return a - b;
 		}
+	public int multiplicar(int a, int b) {
+		 return a * b;
+		}
+	public double dividir(int a, int b) {
+		 return (double) a / b;
+		}
+
 }
